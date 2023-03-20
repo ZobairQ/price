bf9d88ee8d348329eccce8897623c88925438ffc
Zobair Qauomi <zoq@cego.dk>
2023-03-20 16:57:45 +0100

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,47 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { useState } from "react";
+import { ChakraProvider, Box, Text, Grid, theme, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+import axios from "axios";
+const SERVER_ADDRESS = process.env.SERVER_ADDRESS || "localhost:8080";
+export const App = () => {
+  const [companyName, setCompanyName] = useState("");
+  const [productName, setProductName] = useState("");
+  const [price, setPrice] = useState("");
+  const sendData = () => {
+    axios.get(`${SERVER_ADDRESS}`, {
+      params: {
+        company: companyName,
+        productName,
+        price,
+      },
+    });
+  };
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Text color="gray.600" mt={4}>
+          Pris information
+        </Text>
+        <Grid minH="100vh" p={8}>
+          <FormControl>
+            <FormLabel color="gray.600">Producentens navn</FormLabel>
+            <Input type="text" onChange={(e) => setCompanyName(e.target.value)} />
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+            <FormLabel mt={8} color="gray.600">
+              Produktets navn
+            </FormLabel>
+            <Input type="text" onChange={(e) => setProductName(e.target.value)} />
+
+            <FormLabel mt={8} color="gray.600">
+              Produktets Pris
+            </FormLabel>
+            <Input type="number" onChange={(e) => setPrice(e.target.value)} />
+
+            <Button mt={12} padding={12} colorScheme="green" isLoading={false} type="button" onClick={sendData}>
+              Print Prisen
+            </Button>
+          </FormControl>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+};
